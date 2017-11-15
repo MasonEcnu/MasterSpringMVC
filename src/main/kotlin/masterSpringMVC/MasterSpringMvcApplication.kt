bfd1c5dc37a0175f6a6cea70e750f6dc2a69f7eb
Coder_Mason <364206176@qq.com>
2017-11-15 22:44:11 +0800

    spring mvc starter

---
+++ src/main/kotlin/masterSpringMVC/MasterSpringMvcApplication.kt
@@ -0,0 +1,11 @@
+package masterSpringMVC
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MasterSpringMvcApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(MasterSpringMvcApplication::class.java, *args)
+}
